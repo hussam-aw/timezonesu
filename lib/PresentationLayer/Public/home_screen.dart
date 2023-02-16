b99e36af5f42c9f1c143a1b370f03ebabb044e06
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-16 06:08:36 -0800

    Add bottom navigation bar

--- lib/PresentationLayer/Public/home_screen.dart
+++ lib/PresentationLayer/Public/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timezonesu/Constants/ui_colors.dart';
+import 'package:timezonesu/PresentationLayer/Widgets/Public/bottomNavigationBar.dart';
 import 'package:timezonesu/PresentationLayer/Widgets/Public/drawer.dart';
 import 'package:timezonesu/PresentationLayer/Widgets/Public/timezome_appbar.dart';
 
@@ -9,31 +10,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Directionality(
-        textDirection: TextDirection.ltr,
-        child: Scaffold(
-          appBar: tzAppBar(
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.shopping_cart,
-                  size: 30,
-                  color: UIColors.activeIcon,
-                ),
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        appBar: tzAppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.shopping_cart,
+                size: 30,
+                color: UIColors.activeIcon,
               ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.search,
-                  size: 30,
-                  color: UIColors.activeIcon,
-                ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 30,
+                color: UIColors.activeIcon,
               ),
-              const SizedBox(width: 8),
-            ],
-          ),
-          drawer: const TzDrawer(),
-          body: const SizedBox(),
-        ));
+            ),
+            const SizedBox(width: 8),
+          ],
+        ),
+        drawer: const TzDrawer(),
+        body: const SizedBox(),
+        bottomNavigationBar: const TZBottomNavigationBar(),
+      ),
+    );
   }
 }
