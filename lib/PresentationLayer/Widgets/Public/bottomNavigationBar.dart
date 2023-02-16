248c6a8bea73b910bc2c9bb6c45413af9e027642
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-16 06:08:06 -0800

    Create custom bottom navigation bar for app

---
+++ lib/PresentationLayer/Widgets/Public/bottomNavigationBar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:timezonesu/Constants/ui_colors.dart';
+
+class TZBottomNavigationBar extends StatelessWidget {
+  const TZBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: UIColors.primary,
+      selectedItemColor: UIColors.activeIcon,
+      selectedFontSize: 12,
+      unselectedItemColor: UIColors.inActiveIcon,
+      unselectedFontSize: 12,
+      currentIndex: 0,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            size: 32,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.notifications,
+          ),
+          label: 'Notifications',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.shopping_cart_outlined,
+          ),
+          label: 'My Bag',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.star_border,
+          ),
+          label: 'Favourites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+          ),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
