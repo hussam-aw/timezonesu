84d785ccaaee8d20debb7fb75734fd07130f1f13
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-18 00:55:14 -0800

    Create splash screen design

--- lib/PresentationLayer/Public/splash_screen.dart
+++ lib/PresentationLayer/Public/splash_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
-class SplashScreen extends StatelessWidget 
-{
+import 'package:timezonesu/Constants/ui_colors.dart';
+
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+      bottomSheet: LinearProgressIndicator(
+        value: 0.7,
+        minHeight: 14,
+        backgroundColor: UIColors.defaultProgress,
+        valueColor: AlwaysStoppedAnimation<Color>(UIColors.primary),
+      ),
+    );
   }
 }
