3f3418ca74b45382e7c87c2a9a3eccab42d1ad96
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-18 04:18:54 -0800

    Update font family and font size

--- lib/Constants/ui_text_style.dart
+++ lib/Constants/ui_text_style.dart
@@ -3,52 +3,57 @@ import 'package:flutter/material.dart';
 class UITextStyle {
   static const boldHeading = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",   height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.bold,
       fontSize: 20.0);
 
   static const normalHeading = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",
-         height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.normal,
       fontSize: 20.0);
 
   static const boldMeduim = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",
-         height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.bold,
-      fontSize: 13.0);
+      fontSize: 16.0);
 
   static const normalMeduim = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",
+      fontFamily: "Red Hat Display",
       height: 1,
       fontWeight: FontWeight.normal,
-      fontSize: 13.0);
+      fontSize: 16.0);
 
   static const boldBody = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",   height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.bold,
       fontSize: 12.0);
 
   static const normalBody = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",   height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.normal,
       fontSize: 12.0);
 
   static const boldSmall = TextStyle(
       color: Colors.white,
-      fontFamily: "Cairo",   height: 1,
+      fontFamily: "Red Hat Display",
+      height: 1,
       fontWeight: FontWeight.bold,
       fontSize: 8.0);
 
   static const normalSmall = TextStyle(
-      color: Colors.white,   height: 1,
-      fontFamily: "Cairo",
+      color: Colors.white,
+      height: 1,
+      fontFamily: "Red Hat Display",
       fontWeight: FontWeight.normal,
       fontSize: 8.0);
 }
