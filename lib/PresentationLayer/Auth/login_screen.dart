161136e132bc4848e81602ec78e13aea11619511
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-18 04:48:35 -0800

    Create login screen design

--- lib/PresentationLayer/Auth/login_screen.dart
+++ lib/PresentationLayer/Auth/login_screen.dart
@@ -1,10 +1,169 @@
-
 import 'package:flutter/material.dart';
+import 'package:timezonesu/Constants/ui_colors.dart';
+import 'package:timezonesu/Constants/ui_styles.dart';
+import 'package:timezonesu/Constants/ui_text_style.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                ClipPath(
+                  clipper: HeaderClipPath(),
+                  child: Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: UIColors.primary,
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Welcome Back',
+                            style: UITextStyle.boldHeading,
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            'Please Sign In To Your Account',
+                            style: UITextStyle.normalHeading,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: authInputsStyle.copyWith(
+                            hintText: "Enter Your Email",
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: authInputsStyle.copyWith(
+                            hintText: 'Enter Your Password',
+                            suffixIcon: Icon(Icons.visibility),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Forget Password?',
+                              style: UITextStyle.normalMeduim.copyWith(
+                                color: UIColors.darknormalText,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Continue',
+                              style: UITextStyle.normalMeduim,
+                            ),
+                            style: acceptButtonStyle,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset('assets/icons/google.png'),
+                                SizedBox(width: 8),
+                                Text(
+                                  'Continue With Google',
+                                  style: UITextStyle.normalMeduim
+                                      .copyWith(color: UIColors.darknormalText),
+                                ),
+                              ],
+                            ),
+                            style: normalButtonWithBorderStyle,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account ? ',
+                              style: UITextStyle.normalMeduim.copyWith(
+                                color: UIColors.darknormalText,
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text(
+                                'Create One',
+                                style: UITextStyle.normalMeduim.copyWith(
+                                  color: UIColors.darknormalText,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class HeaderClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 50);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstPoint = Offset(size.width / 2, size.height);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstPoint.dx, firstPoint.dy);
+
+    var secondControlPoint = Offset(size.width - (size.width / 4), size.height);
+    var secondPoint = Offset(size.width, size.height - 50);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondPoint.dx, secondPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
