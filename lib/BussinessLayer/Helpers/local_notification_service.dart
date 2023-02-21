cc3d1bd72aa70cd6330462e0ecfca6856d9997ad
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-21 05:55:49 -0800

    Prepare app for push notifications

--- lib/BussinessLayer/Helpers/local_notification_service.dart
+++ lib/BussinessLayer/Helpers/local_notification_service.dart
@@ -1,1 +1,38 @@
-class LocalNotificationService {}
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  static void initialize() {
+    //Initialization settings
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher.png"),
+    );
+    _notificationsPlugin.initialize(initializationSettings);
+  }
+
+  static void createAndDisplayNotification(RemoteMessage message) async {
+    try {
+      final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          "timezonesu",
+          "timezonesuchannel",
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
+      );
+      await _notificationsPlugin.show(
+        id,
+        message.notification!.title,
+        message.notification!.body,
+        notificationDetails,
+        payload: message.data['_id'],
+      );
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+}
