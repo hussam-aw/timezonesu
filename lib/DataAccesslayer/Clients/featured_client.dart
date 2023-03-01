b8a09f8ca49521cea617464b5efe2ba7c2016350
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-01 00:33:12 -0800

    Use featured products link

--- lib/DataAccesslayer/Clients/featured_client.dart
+++ lib/DataAccesslayer/Clients/featured_client.dart
@@ -2,10 +2,9 @@ import 'package:http/http.dart' as http;
 
 import '../../Constants/api_links.dart';
 
-
 class FeaturedClient {
   Future<dynamic> getFeatured() async {
-    var response = await http.get(Uri.parse(baseUrlV1 + "featureProducts"));
+    var response = await http.get(Uri.parse(baseUrlV1 + featuredProductsLink));
     print(response.body);
     if (response.statusCode == 200) {
       return response.body;
