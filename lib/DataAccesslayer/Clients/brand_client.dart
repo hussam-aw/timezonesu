51366e6e796557a98e8f49310f8f77bb90bb8f90
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-01 00:21:12 -0800

    Use brands link

--- lib/DataAccesslayer/Clients/brand_client.dart
+++ lib/DataAccesslayer/Clients/brand_client.dart
@@ -1,10 +1,9 @@
 import 'package:http/http.dart' as http;
 import '../../Constants/api_links.dart';
 
-
 class BrandsClient {
   Future<dynamic> getBrands() async {
-    var response = await http.get(Uri.parse(baseUrlV1 + "brands"));
+    var response = await http.get(Uri.parse(baseUrlV1 + brandsLink));
     print(response.body);
     if (response.statusCode == 200) {
       return response.body;
