7032097b6fa321f36d1938d84e98abfa6abda987
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-01 00:54:18 -0800

    Add adverts link

--- lib/Constants/api_links.dart
+++ lib/Constants/api_links.dart
@@ -2,7 +2,8 @@ const baseUrl = "http://timezone.brain.sy/api";
 const baseUrlV1 = "http://timezone.brain.sy/api/v1";
 //TODO ALI Add Links From Api Docs
 const exampleLink = "examples";
-const loginLink = "login";
+const loginLink = "/login";
+const advertsLink = "/adverts";
 const categoriesLink = "/categories";
 const brandsLink = "/brands";
 const featuredProductsLink = "/featureProducts";
