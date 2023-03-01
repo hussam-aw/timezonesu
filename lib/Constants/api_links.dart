43bfe0ff987665c14692a6e976ddd253e3131e7d
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-28 23:57:09 -0800

    Correct api link

--- lib/Constants/api_links.dart
+++ lib/Constants/api_links.dart
@@ -1,5 +1,5 @@
 const baseUrl = "http://timezone.brain.sy/api";
-const baseUrlV1 = "http://timezone.brain.sy/api/V1";
+const baseUrlV1 = "http://timezone.brain.sy/api/v1";
 //TODO ALI Add Links From Api Docs
 const exampleLink = "examples";
 const loginLink = "login";
