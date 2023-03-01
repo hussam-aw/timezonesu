7b371a23227f714bccc25c8a4e2dcec2655954fe
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-28 23:48:05 -0800

    Add get categories from repo

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -2,4 +2,10 @@ import 'package:get/get.dart';
 import 'package:timezonesu/DataAccesslayer/Models/category.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/category_repo.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  CategoriesRepo categoriesRepo = CategoriesRepo();
+
+  Future<List<Category>> getCategories() async {
+    return await categoriesRepo.myCategories();
+  }
+}
