8a8715678a498c8d6cea9ceeee53c6ca105fee7f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-01 00:21:34 -0800

    Add get brands

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
+import 'package:timezonesu/DataAccesslayer/Models/brand.dart';
 import 'package:timezonesu/DataAccesslayer/Models/category.dart';
+import 'package:timezonesu/DataAccesslayer/Repositories/brand_repo.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/category_repo.dart';
 
 class HomeController extends GetxController {
   CategoriesRepo categoriesRepo = CategoriesRepo();
   List<Category> categories = [];
+
+  BrandsRepo brandsRepo = BrandsRepo();
+  List<Brand> brands = [];
 
   Future<void> getCategories() async {
     categories = await categoriesRepo.myCategories();
@@ -12,9 +17,15 @@ class HomeController extends GetxController {
     update();
   }
 
+  Future<void> getBrands() async {
+    brands = await brandsRepo.myBrands();
+    print(brands);
+  }
+
   @override
   void onInit() {
     getCategories();
+    getBrands();
     super.onInit();
   }
 }
