23339087c1cde12f43523ac7260d04fa426f3ddf
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-28 23:31:27 -0800

    Add categories link

--- lib/DataAccesslayer/Clients/category_client.dart
+++ lib/DataAccesslayer/Clients/category_client.dart
@@ -2,10 +2,9 @@ import 'package:http/http.dart' as http;
 
 import '../../Constants/api_links.dart';
 
-
 class CategoriesClient {
   Future<dynamic> getCategories() async {
-    var response = await http.get(Uri.parse(baseUrlV1 + "categories"));
+    var response = await http.get(Uri.parse(baseUrlV1 + categoriesLink));
     print(response.body);
     if (response.statusCode == 200) {
       return response.body;
