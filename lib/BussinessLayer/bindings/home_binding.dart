ce0223b834ef96ddd931f2e9ca6610e4b130e1ea
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-28 23:46:31 -0800

    Add home controller binding

--- lib/BussinessLayer/bindings/home_binding.dart
+++ lib/BussinessLayer/bindings/home_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:timezonesu/BussinessLayer/Controllers/home_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put(HomeController());
+  }
 }
