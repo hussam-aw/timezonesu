d57b68e9a946bb7499c2123dd2f7fae75cd8b94b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-01 00:42:04 -0800

    Add get featured products from repo

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
 import 'package:timezonesu/DataAccesslayer/Models/brand.dart';
 import 'package:timezonesu/DataAccesslayer/Models/category.dart';
+import 'package:timezonesu/DataAccesslayer/Models/featured_product.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/brand_repo.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/category_repo.dart';
+import 'package:timezonesu/DataAccesslayer/Repositories/featured_repo.dart';
 
 class HomeController extends GetxController {
   CategoriesRepo categoriesRepo = CategoriesRepo();
@@ -10,6 +12,9 @@ class HomeController extends GetxController {
 
   BrandsRepo brandsRepo = BrandsRepo();
   List<Brand> brands = [];
+
+  FeaturedRepo featuredRepo = FeaturedRepo();
+  List<FeaturedProduct> featuredProducts = [];
 
   Future<void> getCategories() async {
     categories = await categoriesRepo.myCategories();
@@ -23,10 +28,16 @@ class HomeController extends GetxController {
     update();
   }
 
+  Future<void> getFeaturedProducts() async {
+    featuredProducts = await featuredRepo.myFeaturedProducts();
+    print(featuredProducts);
+  }
+
   @override
   void onInit() {
     getCategories();
     getBrands();
+    getFeaturedProducts();
     super.onInit();
   }
 }
