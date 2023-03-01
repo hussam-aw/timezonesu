77ba6607e9c93c46896e32efddf36dccb607b5b2
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-02-28 23:46:18 -0800

    Create home controller

---
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+import 'package:timezonesu/DataAccesslayer/Models/category.dart';
+import 'package:timezonesu/DataAccesslayer/Repositories/category_repo.dart';
+
+class HomeController extends GetxController {}
