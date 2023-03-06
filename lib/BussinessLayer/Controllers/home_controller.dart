b2246eefec52bfc2eb90ba52e560a2fbb1551c5f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-06 00:48:48 -0800

    Add isLoading as obs variable to check fetching data

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -7,6 +7,8 @@ import 'package:timezonesu/DataAccesslayer/Repositories/category_repo.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/featured_repo.dart';
 
 class HomeController extends GetxController {
+  var isLoading = false.obs;
+
   CategoriesRepo categoriesRepo = CategoriesRepo();
   List<Category> categories = [];
 
@@ -19,26 +21,36 @@ class HomeController extends GetxController {
   Future<void> getCategories() async {
     categories = await categoriesRepo.myCategories();
     print(categories);
-    update();
   }
 
   Future<void> getBrands() async {
     brands = await brandsRepo.myBrands();
     print(brands);
-    update();
   }
 
   Future<void> getFeaturedProducts() async {
     featuredProducts = await featuredRepo.myFeaturedProducts();
     print(featuredProducts);
-    update();
+  }
+
+  void fetchHomeData() async {
+    isLoading(true);
+    await getCategories();
+    await getBrands();
+    await getFeaturedProducts();
+    if (categories.isNotEmpty &&
+        brands.isNotEmpty &&
+        featuredProducts.isNotEmpty) {
+      update();
+      isLoading(false);
+    }
+
+    print(isLoading.value);
   }
 
   @override
   void onInit() {
-    getCategories();
-    getBrands();
-    getFeaturedProducts();
+    fetchHomeData();
     super.onInit();
   }
 }
