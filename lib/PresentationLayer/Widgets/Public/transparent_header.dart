e0cadaf3ca6a84f539e3218c78b3a35e06f4beba
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-07 01:36:11 -0800

    Create transparent header to category and product screen

---
+++ lib/PresentationLayer/Widgets/Public/transparent_header.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:timezonesu/Constants/ui_colors.dart';
+
+class TransparentHeader extends StatelessWidget {
+  TransparentHeader({super.key, this.trailling});
+
+  Widget? trailling;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            UIColors.darkGrey,
+            UIColors.white.withOpacity(0.1),
+          ],
+          stops: const [0.0, 1.0],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () => Get.back(),
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                size: 30,
+                color: UIColors.activeIcon,
+              ),
+            ),
+            trailling ?? Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
