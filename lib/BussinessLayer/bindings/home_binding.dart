ab866abad63533cee384404cbe7e6affcc20a844
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-07 02:59:37 -0800

    Add category controller binding

--- lib/BussinessLayer/bindings/home_binding.dart
+++ lib/BussinessLayer/bindings/home_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'package:timezonesu/BussinessLayer/Controllers/category_controller.dart';
 import 'package:timezonesu/BussinessLayer/Controllers/home_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(HomeController());
+    Get.lazyPut(() => CategoryController());
   }
 }
