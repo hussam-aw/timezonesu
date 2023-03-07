0d6a67efec9528e4188fefd57a22c12881dfe6f5
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-07 03:00:27 -0800

    Add get category products function

--- lib/BussinessLayer/Controllers/category_controller.dart
+++ lib/BussinessLayer/Controllers/category_controller.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
+import 'package:timezonesu/DataAccesslayer/Models/product.dart';
+import 'package:timezonesu/DataAccesslayer/Repositories/ProductsByCategoryRepo.dart';
 
 class CategoryController extends GetxController {
+  ProductsByCategoryRepo productsByCategoryRepo = ProductsByCategoryRepo();
+  List<Product> products = [];
+
+  void getCategoryProducts(int categoryId) async {
+    products = await productsByCategoryRepo.products(categoryId);
+  }
+
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
   }
 }
