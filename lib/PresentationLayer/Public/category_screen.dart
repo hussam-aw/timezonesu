397adb79815ee9c8312e128a52f35cbd8a988e28
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-07 04:46:57 -0800

    Update category screen

--- lib/PresentationLayer/Public/category_screen.dart
+++ lib/PresentationLayer/Public/category_screen.dart
@@ -21,78 +21,85 @@ class CategoryScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: UIColors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Stack(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 9,
+              child: Column(
                 children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(18),
-                      bottomRight: Radius.circular(18),
-                    ),
-                    child: SizedBox(
-                      height: 300,
-                      width: Get.width,
-                      child: Image.network(
-                        category.image,
-                        fit: BoxFit.cover,
+                  Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(18),
+                          bottomRight: Radius.circular(18),
+                        ),
+                        child: SizedBox(
+                          height: 300,
+                          width: Get.width,
+                          child: Image.network(
+                            category.image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ),
+                      TransparentHeader(
+                        trailling: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.shopping_cart,
+                            size: 30,
+                            color: UIColors.activeIcon,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  TransparentHeader(
-                    trailling: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.shopping_cart,
-                        size: 30,
-                        color: UIColors.activeIcon,
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            child: Text(
+                              category.name,
+                              style: UITextStyle.boldHeading.copyWith(
+                                color: UIColors.subTitle,
+                              ),
+                            ),
+                          ),
+                          GetBuilder<CategoryController>(builder: (context) {
+                            return Container(
+                              height: 300,
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: ListView.separated(
+                                itemBuilder: (context, index) {
+                                  return CategoryProductBox(
+                                    brandName: categoryProducts[index].brand,
+                                    productName: categoryProducts[index].name,
+                                    price: categoryProducts[index].price,
+                                    offer: categoryProducts[index].offer,
+                                    image: categoryProducts[index].images[0],
+                                  );
+                                },
+                                separatorBuilder: (context, index) {
+                                  return spacer(height: 20);
+                                },
+                                itemCount: categoryProducts.length,
+                              ),
+                            );
+                          }),
+                        ],
                       ),
                     ),
                   ),
                 ],
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: Text(
-                        category.name,
-                        style: UITextStyle.boldHeading.copyWith(
-                          color: UIColors.subTitle,
-                        ),
-                      ),
-                    ),
-                    GetBuilder<CategoryController>(builder: (context) {
-                      return Container(
-                        height: 360,
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: ListView.separated(
-                          itemBuilder: (context, index) {
-                            return CategoryProductBox(
-                              brandName: categoryProducts[index].brand,
-                              productName: categoryProducts[index].name,
-                              price: categoryProducts[index].price,
-                              offer: categoryProducts[index].offer,
-                              image: categoryProducts[index].images[0],
-                            );
-                          },
-                          separatorBuilder: (context, index) {
-                            return spacer(height: 20);
-                          },
-                          itemCount: categoryProducts.length,
-                        ),
-                      );
-                    }),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: TZBottomNavigationBar(),
