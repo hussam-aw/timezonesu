ba6e53a17bb31f477c1ecd7c3dc37cc95fab2710
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-08 23:19:18 -0800

    Make login screen responsive

--- lib/PresentationLayer/Auth/login_screen.dart
+++ lib/PresentationLayer/Auth/login_screen.dart
@@ -12,141 +12,139 @@ class LoginScreen extends StatelessWidget {
   final AuthController authController = Get.put(AuthController());
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 4,
-              child: ClipPath(
-                clipper: HeaderClipPath(),
-                child: Container(
-                  height: 200,
-                  width: Get.width,
-                  decoration: const BoxDecoration(
-                    color: UIColors.primary,
+        child: SizedBox(
+          width: width,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 4,
+                child: ClipPath(
+                  clipper: HeaderClipPath(),
+                  child: Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: UIColors.primary,
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Welcome Back',
+                            style: UITextStyle.boldHeading,
+                          ),
+                          spacer(height: 16),
+                          const Text(
+                            'Please Sign In To Your Account',
+                            style: UITextStyle.normalHeading,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Center(
+                ),
+              ),
+              Expanded(
+                flex: 9,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  child: Form(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          'Welcome Back',
-                          style: UITextStyle.boldHeading,
+                        TextFormField(
+                          controller: authController.loginEmailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: authInputsStyle.copyWith(
+                            hintText: "Enter Your Email",
+                          ),
                         ),
-                        spacer(height: 16),
-                        const Text(
-                          'Please Sign In To Your Account',
-                          style: UITextStyle.normalHeading,
-                        )
+                        spacer(height: 20),
+                        TextFormField(
+                          controller: authController.loginPasswordController,
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: authInputsStyle.copyWith(
+                            hintText: 'Enter Your Password',
+                            suffixIcon: const Icon(Icons.visibility),
+                          ),
+                        ),
+                        spacer(height: 15),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Text(
+                              'Forget Password?',
+                              style: UITextStyle.normalMeduim.copyWith(
+                                color: UIColors.darknormalText,
+                              ),
+                            ),
+                          ),
+                        ),
+                        spacer(height: 50),
+                        SizedBox(
+                          width: Get.width,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await authController.login();
+                            },
+                            style: acceptButtonStyle,
+                            child: const Text(
+                              'Continue',
+                              style: UITextStyle.normalMeduim,
+                            ),
+                          ),
+                        ),
+                        spacer(),
+                        SizedBox(
+                          width: Get.width,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.toNamed(AppRoutes.homepage);
+                            },
+                            style: normalButtonWithBorderStyle,
+                            child: Text(
+                              'Skip',
+                              style: UITextStyle.normalMeduim.copyWith(
+                                color: UIColors.darknormalText,
+                              ),
+                            ),
+                          ),
+                        ),
+                        spacer(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account ? ',
+                              style: UITextStyle.normalMeduim.copyWith(
+                                color: UIColors.darknormalText,
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                authController.goToRegister();
+                              },
+                              child: Text(
+                                'Create One',
+                                style: UITextStyle.normalMeduim.copyWith(
+                                  color: UIColors.darknormalText,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
                   ),
                 ),
               ),
-            ),
-            //spacer(height: 30),
-            Expanded(
-              flex: 9,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: authController.loginEmailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: authInputsStyle.copyWith(
-                          hintText: "Enter Your Email",
-                        ),
-                      ),
-                      spacer(height: 20),
-                      TextFormField(
-                        controller: authController.loginPasswordController,
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: authInputsStyle.copyWith(
-                          hintText: 'Enter Your Password',
-                          suffixIcon: const Icon(Icons.visibility),
-                        ),
-                      ),
-                      spacer(height: 15),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: InkWell(
-                          onTap: () {},
-                          child: Text(
-                            'Forget Password?',
-                            style: UITextStyle.normalMeduim.copyWith(
-                              color: UIColors.darknormalText,
-                            ),
-                          ),
-                        ),
-                      ),
-                      spacer(height: 50),
-                      SizedBox(
-                        width: Get.width,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await authController.login();
-                          },
-                          style: acceptButtonStyle,
-                          child: const Text(
-                            'Continue',
-                            style: UITextStyle.normalMeduim,
-                          ),
-                        ),
-                      ),
-                      spacer(),
-                      SizedBox(
-                        width: Get.width,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Get.toNamed(AppRoutes.homepage);
-                          },
-                          style: normalButtonWithBorderStyle,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Skip',
-                                style: UITextStyle.normalMeduim
-                                    .copyWith(color: UIColors.darknormalText),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      spacer(height: 15),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Don\'t have an account ? ',
-                            style: UITextStyle.normalMeduim.copyWith(
-                              color: UIColors.darknormalText,
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              authController.goToRegister();
-                            },
-                            child: Text(
-                              'Create One',
-                              style: UITextStyle.normalMeduim.copyWith(
-                                color: UIColors.darknormalText,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
