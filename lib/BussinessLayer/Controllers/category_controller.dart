f88f4a87088dfc6ac72fbca59b3f2de47803213f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-10 02:16:32 -0800

    Fix some issues in category controller

--- lib/BussinessLayer/Controllers/category_controller.dart
+++ lib/BussinessLayer/Controllers/category_controller.dart
@@ -2,11 +2,24 @@ import 'package:get/get.dart';
 import 'package:timezonesu/DataAccesslayer/Models/product.dart';
 import 'package:timezonesu/DataAccesslayer/Repositories/products_bycategory_repo.dart';
 
+import '../../DataAccesslayer/Models/category.dart';
+
 class CategoryController extends GetxController {
+  final Category category;
+
   ProductsByCategoryRepo productsByCategoryRepo = ProductsByCategoryRepo();
   List<Product> products = [];
 
+  CategoryController(this.category);
+
   Future<void> getCategoryProducts(int categoryId) async {
     products = await productsByCategoryRepo.products(categoryId);
+    update();
+  }
+
+  @override
+  void onInit() async {
+    await getCategoryProducts(category.id);
+    super.onInit();
   }
 }
