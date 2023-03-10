520ba62ce03ebc5fada430f9a719f48394064a40
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-10 02:10:19 -0800

    Make widget responsive

--- lib/PresentationLayer/Widgets/shimmers/home_category_shimmer.dart
+++ lib/PresentationLayer/Widgets/shimmers/home_category_shimmer.dart
@@ -3,11 +3,26 @@ import 'package:timezonesu/PresentationLayer/Widgets/Public/spaces.dart';
 import 'package:timezonesu/PresentationLayer/Widgets/shimmers/base_shimmer.dart';
 
 Widget homeCategoryShimmer() {
-  return Column(
-    children: [
-      const Shimmerwidget.roundedrectangler(width: 85, height: 85),
-      spacer(),
-      const Shimmerwidget.roundedrectangler(width: 85, height: 10)
-    ],
+  return SizedBox(
+    height: 112,
+    child: Column(
+      children: [
+        const Expanded(
+          flex: 4,
+          child: Shimmerwidget.roundedrectangler(
+            width: 85,
+            height: 88,
+          ),
+        ),
+        spacer(),
+        const Expanded(
+          flex: 1,
+          child: Shimmerwidget.roundedrectangler(
+            width: 85,
+            height: 24,
+          ),
+        )
+      ],
+    ),
   );
 }
