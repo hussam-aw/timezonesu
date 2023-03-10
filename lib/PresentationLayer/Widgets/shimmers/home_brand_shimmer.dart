bec7548c5dbf1bca3daac96187d2dd6bd25d0c12
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-10 03:25:50 -0800

    Add home brand shimmer widget

---
+++ lib/PresentationLayer/Widgets/shimmers/home_brand_shimmer.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:timezonesu/PresentationLayer/Widgets/shimmers/base_shimmer.dart';
+
+Widget homeBrandShimmer() {
+  return Shimmerwidget.circular(
+    width: 60,
+    height: 65,
+  );
+}
