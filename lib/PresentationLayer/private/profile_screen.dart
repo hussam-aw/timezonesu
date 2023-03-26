fdbc63f376f114884bfde5c87e91cdf849c9b7ab
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-26 05:01:52 -0700

    Finish profile screen and use MyApp.user to fetch user data

--- lib/PresentationLayer/private/profile_screen.dart
+++ lib/PresentationLayer/private/profile_screen.dart
@@ -3,12 +3,15 @@ import 'package:get/get.dart';
 import 'package:timezonesu/Constants/ui_colors.dart';
 import 'package:timezonesu/Constants/ui_styles.dart';
 import 'package:timezonesu/Constants/ui_text_style.dart';
+import 'package:timezonesu/DataAccesslayer/Models/user.dart';
 import 'package:timezonesu/PresentationLayer/Widgets/Public/spaces.dart';
 import 'package:timezonesu/PresentationLayer/Widgets/Public/timezome_appbar.dart';
+import 'package:timezonesu/main.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  ProfileScreen({super.key});
 
+  User? user = MyApp.appUser;
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
@@ -46,7 +49,7 @@ class ProfileScreen extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(vertical: 15),
                     child: Column(
                       children: [
-                        const Expanded(
+                        Expanded(
                           flex: 4,
                           child: CircleAvatar(
                             minRadius: 68,
@@ -54,6 +57,7 @@ class ProfileScreen extends StatelessWidget {
                             child: CircleAvatar(
                               minRadius: 63,
                               backgroundColor: UIColors.circleAvatarBackground,
+                              backgroundImage: NetworkImage(user!.avatar),
                             ),
                           ),
                         ),
@@ -61,7 +65,7 @@ class ProfileScreen extends StatelessWidget {
                         Expanded(
                           flex: 1,
                           child: Text(
-                            'Ali Alsaeed',
+                            user!.name,
                             style: UITextStyle.boldMeduim.copyWith(
                               color: UIColors.normalText,
                             ),
@@ -81,21 +85,33 @@ class ProfileScreen extends StatelessWidget {
                           child: Column(
                             children: [
                               TextFormField(
+                                enabled: false,
                                 keyboardType: TextInputType.name,
-                                decoration: profileInputsStyle,
+                                decoration: profileInputsStyle.copyWith(
+                                  hintText: user!.name,
+                                ),
                               ),
                               spacer(height: 14),
                               TextFormField(
-                                decoration: profileInputsStyle,
+                                enabled: false,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: profileInputsStyle.copyWith(
+                                  hintText: user!.email,
+                                ),
                               ),
                               spacer(height: 14),
                               TextFormField(
-                                decoration: profileInputsStyle,
+                                keyboardType: TextInputType.name,
+                                decoration: profileInputsStyle.copyWith(
+                                  hintText: 'Leave It Empty To Avoid Change',
+                                ),
                               ),
                               spacer(height: 14),
                               TextFormField(
                                 keyboardType: TextInputType.emailAddress,
-                                decoration: profileInputsStyle,
+                                decoration: profileInputsStyle.copyWith(
+                                  hintText: 'Enter Your Adress',
+                                ),
                               ),
                             ],
                           ),
