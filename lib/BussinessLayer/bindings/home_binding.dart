4af472139cd70044ac985e2197997bc8e71a5655
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-26 07:54:30 -0700

    Add profile controller binding

--- lib/BussinessLayer/bindings/home_binding.dart
+++ lib/BussinessLayer/bindings/home_binding.dart
@@ -4,6 +4,7 @@ import 'package:timezonesu/BussinessLayer/Controllers/favorites_controller.dart'
 import 'package:timezonesu/BussinessLayer/Controllers/home_controller.dart';
 import 'package:timezonesu/BussinessLayer/Controllers/notification_controller.dart';
 import 'package:timezonesu/BussinessLayer/Controllers/product_controller.dart';
+import 'package:timezonesu/BussinessLayer/Controllers/profile_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
@@ -13,5 +14,6 @@ class HomeBindings extends Bindings {
     Get.put(CartController());
     Get.put(FavouriteController());
     Get.put(BackNotificationController());
+    Get.lazyPut(() => ProfileController(), fenix: true);
   }
 }
