929184a5eb3befc1e25bbe65ded1bd744d1b7b64
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-27 03:12:14 -0700

    Put AppLanguageController dependency

--- lib/BussinessLayer/bindings/init_binding.dart
+++ lib/BussinessLayer/bindings/init_binding.dart
@@ -1,3 +1,9 @@
-class InitBinding  {
-  
+import 'package:get/get.dart';
+import 'package:timezonesu/BussinessLayer/Controllers/app_language_controller.dart';
+
+class InitBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => AppLanguageController(), fenix: true);
+  }
 }
