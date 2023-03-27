7061130637c4c476d44d3e39b1d584030d60e413
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-27 00:29:32 -0700

    Add some strings

---
+++ lib/BussinessLayer/Helpers/langs/ar.dart
@@ -0,0 +1,11 @@
+const Map<String, String> ar = {
+  'welcomeBack': 'أهلا بك مجددا',
+  'pleaseSignInToYourAccount': 'يرجى تسجيل الدخول الى الحساب الخاص بك',
+  'enterYourEmail': 'أدخل البريد الالكتروني الخاص بك',
+  'enterYourPassword': 'أدخل كلمة السر الخاصة بك',
+  'forgetPassword?': 'هل نسيت كلمة السر؟',
+  'continue': 'متابعة',
+  'continueWithGoogle': 'متابعة باستخدام غوغل',
+  'don\'tHaveAnAccount?': 'ليس لديك حساب؟',
+  'createOne': 'أنشئ واحدا',
+};
---
+++ lib/BussinessLayer/Helpers/langs/en.dart
@@ -0,0 +1,11 @@
+const Map<String, String> en = {
+  'welcomeBack': 'Welcome Back',
+  'pleaseSignInToYourAccount': 'Please Sign In To Your Account',
+  'enterYourEmail': 'Enter Your Email',
+  'enterYourPassword': 'Enter Your Password',
+  'forgetPassword?': 'Forget Password?',
+  'continue': 'Continue',
+  'continueWithGoogle': 'Continue With Google',
+  'don\'tHaveAnAccount?': 'Don\'t Have an Account?',
+  'createOne': 'Create One',
+};
