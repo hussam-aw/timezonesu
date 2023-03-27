3a3973de41a420c8b495b1a4262bd340e2ab5793
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-27 03:11:53 -0700

    Create change language method

--- lib/BussinessLayer/Controllers/app_language_controller.dart
+++ lib/BussinessLayer/Controllers/app_language_controller.dart
@@ -1,3 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:timezonesu/BussinessLayer/Helpers/box_client.dart';
 
-class AppLanguageController extends GetxController {}
+class AppLanguageController extends GetxController {
+  String? appLang;
+  var appLocale;
+  BoxClient box = BoxClient();
+  @override
+  void onInit() async {
+    super.onInit();
+    appLang = await box.getAppLanguage();
+    appLocale = appLang == null ? Get.deviceLocale : Locale(appLang.toString());
+    update();
+    Get.updateLocale(appLocale);
+  }
+
+  void changeLanguage(String? type) async {
+    if (appLocale == type) {
+      return;
+    }
+    switch (type) {
+      case 'ar':
+        appLang = 'ar';
+        break;
+      case 'en':
+        appLang = 'en';
+        break;
+    }
+    box.setAppLanguage(appLang.toString());
+    update();
+    Get.updateLocale(Locale(type.toString()));
+  }
+}
