fc47a14c82c08b4f51ae12b7967ec2ac91ddb5b3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-29 00:35:18 -0700

    Create banner client class

---
+++ lib/DataAccesslayer/Clients/banner_client.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' as http;
+import 'package:timezonesu/Constants/api_links.dart';
+
+class BannersClient {
+  Future<dynamic> getBanners() async {
+    var response = await http.get(Uri.parse(baseUrlV1 + bannersLink));
+    print(response);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
+}
