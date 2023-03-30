3bc49fdb80afeba97a01a81a5730124b5a9070c9
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-30 01:49:57 -0700

    Add get brands and get big deals methods

--- lib/DataAccesslayer/Repositories/products_repo.dart
+++ lib/DataAccesslayer/Repositories/products_repo.dart
@@ -2,6 +2,8 @@
 
 import 'dart:convert';
 
+import 'package:timezonesu/DataAccesslayer/Models/cart_product.dart';
+
 import '../Clients/products_client.dart';
 import '../Models/product.dart';
 
@@ -13,6 +15,26 @@ class ProductsRepo {
     if (response != "") {
       final parsed = json.decode(response).cast<Map<String, dynamic>>();
       return parsed.map<Product>((json) => Product.fromMap(json)).toList();
+    }
+    return [];
+  }
+
+  Future<List<Product>> brandProducts(brandId) async {
+    var response = await client.getBrandProducts(brandId);
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+      return parsed.map<Product>((json) => Product.fromMap(json)).toList();
+    }
+    return [];
+  }
+
+  Future<List<CartProduct>> bigDealProducts() async {
+    var response = await client.getBigDealProdcts();
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+      return parsed
+          .map<CartProduct>((json) => CartProduct.fromMap(json))
+          .toList();
     }
     return [];
   }
