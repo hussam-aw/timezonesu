d3727adc470ba153ac6c52ec510461f9e4e2d6d0
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-03-30 01:47:56 -0700

    Add onTap field

--- lib/PresentationLayer/Widgets/Home/brand_icon.dart
+++ lib/PresentationLayer/Widgets/Home/brand_icon.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:timezonesu/Constants/ui_colors.dart';
 
 class BrandIcon extends StatelessWidget {
-  const BrandIcon({super.key, this.brandImage});
+  const BrandIcon({
+    super.key,
+    required this.brandImage,
+    required this.onTap,
+  });
 
   final String? brandImage;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(50),
-      child: Container(
-        width: 60,
-        height: 65,
-        decoration: BoxDecoration(
-          color: UIColors.circleAvatarBackground,
-          image: brandImage != null
-              ? DecorationImage(
-                  image: NetworkImage(
-                    brandImage!,
-                  ),
-                  fit: BoxFit.fill)
-              : null,
+    return InkWell(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: Container(
+          width: 60,
+          height: 65,
+          decoration: BoxDecoration(
+            color: UIColors.circleAvatarBackground,
+            image: brandImage != null
+                ? DecorationImage(
+                    image: NetworkImage(
+                      brandImage!,
+                    ),
+                    fit: BoxFit.fill)
+                : null,
+          ),
         ),
       ),
     );
