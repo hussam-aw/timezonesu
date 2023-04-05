cf7ca95e26da85ba4975f4c59177467fac22dd40
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-05 05:04:10 -0700

    Add search method

--- lib/BussinessLayer/Controllers/search_controller.dart
+++ lib/BussinessLayer/Controllers/search_controller.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/state_manager.dart';
+import 'package:timezonesu/Constants/api_links.dart';
 
 import '../../DataAccesslayer/Models/product.dart';
 import '../../DataAccesslayer/Repositories/products_repo.dart';
@@ -6,10 +8,36 @@ import '../../DataAccesslayer/Repositories/products_repo.dart';
 class SearchController extends GetxController {
   var repo = ProductsRepo();
   List<Product> allProducts = [];
+  List<Product> filteredProducts = [];
+  var searchLoading = false.obs;
+
+  void search(String searchText) async {
+    if (searchText.isNotEmpty) {
+      searchLoading.value = true;
+      filteredProducts = allProducts
+          .where((product) =>
+              product.name.toLowerCase() == searchText ||
+              product.category.toLowerCase() == searchText ||
+              product.brand.toLowerCase() == searchText)
+          .toList();
+
+      print(filteredProducts);
+      searchLoading.value = false;
+    } else {
+      filteredProducts.clear();
+    }
+    update();
+  }
 
   @override
   void onInit() async {
     allProducts = await repo.getProducts();
     super.onInit();
+  }
+
+  @override
+  void onClose() {
+    filteredProducts.clear();
+    super.onClose();
   }
 }
