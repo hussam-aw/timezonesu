5dea134f7a5794ec53c05b18e755859c51200234
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-04-06 02:51:29 -0700

    Create get order by id method from api

--- lib/DataAccesslayer/Clients/order_client.dart
+++ lib/DataAccesslayer/Clients/order_client.dart
@@ -48,4 +48,17 @@ class OrderClient {
       return null;
     }
   }
+
+  Future<dynamic> getOrderById(id) async {
+    var response = await http.get(Uri.parse("$baseUrlV1$orderByIdLink/$id"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
 }
